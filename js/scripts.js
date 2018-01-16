e380d9d53539c0fe5c6f1dc1ae35efb0b0f15523
Eric Swotinsky and Manuela Garcia <eric@>
2018-01-16 10:08:45 -0800

    add square bracket notation to access each element in array and display in unordered list.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
   $("form#favThings").submit(function(event) {
     var colorInput = $("input#colorAnswer").val();
     var movieInput = $("input#movieAnswer").val();
-    console.log(colorInput, movieInput);
+    var iceCreamInput = $("input#iceCreamAnswer").val();
+
+    var favArray = [colorInput, movieInput, iceCreamInput];
+    $("#colorResults").text(favArray[0]);
+    $("#movieResults").text(favArray[1]);
+    $("#iceCreamResults").text(favArray[2]);
+
     event.preventDefault();
   });
 });
